fece6d517941ae7ca9da68a5df5d3b921654dbe5
Baozi <13086605818@163.com>
2018-10-19 19:43:54 +0800

    删除排序数组中重复出现的元素

--- differentNumber.java
+++ differentNumber.java
@@ -1,5 +1,11 @@
 package com.LeetCode.com;
-
+/**
+ * 题目：给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 思路：1、按照题目要求把数组中重复的元素删除掉，言外之意就是遇到相同的元素就跳过，不同的元素保留
+ *      2、只有元素不相等的时候才满足要求，但此时需要把满足要求的数字元素存在表示数组新下标的位置
+ * @author Baozi
+ *
+ */
 public class differentNumber {
 	public static void main(String[] args) {
 		int[] a = new int[]{1,2,2,2,2,3,4,4,5,6,6};
